6e3a688d5b8f3a687dca98c923c7a3fc4752baa9
fagadavid <fagan.david3@hotmail.com>
2022-11-02 16:57:35 +0000

    updated project directory

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KAY'] = 'abcdef'
+
+    from .routes import routes
+    from .auth import auth
+
+    app.register_blueprint(routes, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
